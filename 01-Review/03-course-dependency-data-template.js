0a221be04c288b059adc0285bbdb512db3706550
lsminter <lsminteris@gmail.com>
2018-09-10 08:49:22 -0700

    added course dependency data template

---
+++ 01-Review/03-course-dependency-data-template.js
@@ -0,0 +1,23 @@
+{
+  slug: 'react-navigation-for-native-mobile-applications',
+    dependencies: {
+    react: '16.4.0',
+          },
+  reviews: [
+    {
+      performedOn: '2018-06-26',
+      performedBy: 186087,
+      scopeOfReview: 'full course lesson review',
+      notes: [
+        {
+          type: 'major issue',
+          dependency: 'react-navigation',
+          title:
+            '',
+          details:
+            '',
+        },
+      ],
+    },
+  ],
+},
